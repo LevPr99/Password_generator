bdc708b759f6b1bb46e0e408e61bb7b5efdac9eb
LevPr99 <nexuslev@gmail.com>
2023-11-06 22:16:31 +0300

    Работа с парсингом аргументов.

--- passwordArguments.py
+++ passwordArguments.py
@@ -1,13 +1,20 @@
-from argparse import ArgumentParser
+from sys import argv
 
 
 class PasswordArguments:
     
     def __init__(self) -> None:
-        self.parser = ArgumentParser()
+        self.__args = argv[1:]
         self.parser.add_help
 
-    def add_arg(self, arg_name: str, type, help: str, default: int, required: bool = False) -> None:
+    def __parse_args(self):
+        lenght = len(self.__args)
+        if len(self.__args) % 2 == 0:
+            kwargs = dict()
+            for i in self.__args[1:]:
+                kwargs.setdefault(self.__args()[i])
+    
+    def add_arg(self, arg_name: str, help: str, default: int=None, type=int, required: bool = False) -> None:
         self.parser.add_argument(
                                 '-' + arg_name[0], 
                                 '--' + arg_name, 
@@ -15,7 +22,7 @@ class PasswordArguments:
                                 metavar='', 
                                 help=help, 
                                 default=default, 
-                                required=required
+                                required=required,
                                 )
         
     def get_password_arguments(self) -> dict:
--- test1.py
+++
@@ -1,8 +0,0 @@
-from passwordArguments import PasswordArguments
-
-arg = PasswordArguments()
-
-arg.add_arg('length_of_password', type=int, help='''Длина пароля''', default=8)
-arg.add_arg('rows', type=int, help='''Количество строк в выводе''', default=8)
-arg.add_arg('cols', type=int, help='''Количество столбцов в выводе''', default=4)
-print(arg.get_password_arguments())
