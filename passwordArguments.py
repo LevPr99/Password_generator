10c29dcdaabc80835e5546fe019c5d417a22582d
LevPr99 <nexuslev@gmail.com>
2023-11-07 12:26:37 +0300

    1

--- passwordArguments.py
+++ passwordArguments.py
@@ -1,29 +1,25 @@
 from sys import argv
+from typing import Any
 
 
-class PasswordArguments:
+class ArgumentDeskriptor:
     
-    def __init__(self) -> None:
-        self.__args = argv[1:]
-        self.parser.add_help
-
-    def __parse_args(self):
-        lenght = len(self.__args)
-        if len(self.__args) % 2 == 0:
-            kwargs = dict()
-            for i in self.__args[1:]:
-                kwargs.setdefault(self.__args()[i])
+    def __set_name__(self, owner, name):
+        self.name = '__' + name
     
-    def add_arg(self, arg_name: str, help: str, default: int=None, type=int, required: bool = False) -> None:
-        self.parser.add_argument(
-                                '-' + arg_name[0], 
-                                '--' + arg_name, 
-                                type=type, 
-                                metavar='', 
-                                help=help, 
-                                default=default, 
-                                required=required,
-                                )
-        
-    def get_password_arguments(self) -> dict:
-        return self.parser.parse_args().__dict__
+    def __getattribute__(self, obj, __name: str) -> Any:
+        value = getattr(obj, self.name)
+        return value
+    
+    def __setattr__(self, obj, __name: str, __value: Any) -> None:
+        setattr(obj, self.name, __value)
+
+
+class Arguments:
+    
+    args = ArgumentDeskriptor()
+    kwarg = ArgumentDeskriptor()
+    
+    def __init__(self, arg_name: str,) -> None:
+        self.args = argv[1:]
+        self.kwarg
