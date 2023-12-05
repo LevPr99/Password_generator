f0fa62197ceb123d55d6c606826dcd4cdeeaa039
LevPr99 <nexuslev@gmail.com>
2023-12-05 16:30:09 +0300

    мелкие правки

--- pwgen.py
+++ pwgen.py
@@ -1,14 +1,14 @@
 from argparser_wizardx import ArgParser
 from generator_passwd_wizardx import Pwgen
 
-a = ArgParser()
 arg_int = dict(type=int, default=8)
-a.new_argument('ammount', **arg_int)
-a.new_argument('count', **arg_int)
-a.new_argument('letters', action='store_const')
-a.new_argument('punctuation', action='store_const')
-a.new_argument('digits', action='store_const')
-a.set_default_argument('letters')
+arguments = ArgParser()
+arguments.new_argument('ammount', **arg_int)
+arguments.new_argument('count', **arg_int)
+arguments.new_argument('letters', action='store_const')
+arguments.new_argument('punctuation', action='store_const')
+arguments.new_argument('digits', action='store_const')
+arguments.set_default_argument('letters')
 
-p = Pwgen(a.get_arguments())
-p.print_pw()
+passwords = Pwgen(arguments.get_arguments())
+passwords.print_pw()
